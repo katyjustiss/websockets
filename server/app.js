f92ad43ffe780b0fc6ce06aca97bee646e5eeaf1
Katy Justiss <katy.justiss@gmail.com>
2015-08-27 14:49:47 -0500

    monitor client disconnect

--- server/app.js
+++ server/app.js
@@ -15,4 +15,8 @@ var io = socketio(server);
 
 io.on('connection', function (socket) {
   console.log('Client connected:', socket.id);
-})
+
+  socket.on('disconnect', function () {
+    console.log('Client disconnected:', socket.id);
+  });
+});
